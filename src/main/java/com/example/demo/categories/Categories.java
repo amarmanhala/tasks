711e2dedc62ef8bbdb2a922b069ead802e573ae7
amarmanhala <amar.manhala@gmail.com>
2023-09-21 07:40:36 -0400

    Add categories

--- src/main/java/com/example/demo/categories/Categories.java
+++ src/main/java/com/example/demo/categories/Categories.java
@@ -1,17 +1,36 @@
 package com.example.demo.categories;
 
+import java.util.Date;
+
 public class Categories {
 
   private Long id;
-  private String categoryName;
+  private String name;
+  private String description;
+  private String imageUrl;
+  private boolean isActive;
+  private Date createdAt;
+  private Date updatedAt;
 
-  public Categories(Long id, String categoryName) {
+  public Categories(Long id, String name) {
     this.id = id;
-    this.categoryName = categoryName;
+    this.name = name;
   }
 
-  public Categories(String categoryName) {
-    this.categoryName = categoryName;
+  public Categories(Long id, String name, String description, String imageUrl, boolean isActive, Date createdAt,
+      Date updatedAt) {
+    this.id = id;
+    this.name = name;
+    this.description = description;
+    this.imageUrl = imageUrl;
+    this.isActive = isActive;
+    this.createdAt = createdAt;
+    this.updatedAt = updatedAt;
+
+  }
+
+  public Categories(String name) {
+    this.name = name;
   }
 
   public Long getId() {
@@ -22,12 +41,52 @@ public class Categories {
     this.id = id;
   }
 
-  public String getCategoryName() {
-    return categoryName;
+  public String getname() {
+    return name;
   }
 
-  public void setCategoryName(String categoryName) {
-    this.categoryName = categoryName;
+  public void setname(String name) {
+    this.name = name;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public String getImageUrl() {
+    return imageUrl;
+  }
+
+  public void setImageUrl(String imageUrl) {
+    this.imageUrl = imageUrl;
+  }
+
+  public boolean isActive() {
+    return isActive;
+  }
+
+  public void setActive(boolean active) {
+    isActive = active;
+  }
+
+  public Date getCreatedAt() {
+    return createdAt;
+  }
+
+  public void setCreatedAt(Date createdAt) {
+    this.createdAt = createdAt;
+  }
+
+  public Date getUpdatedAt() {
+    return updatedAt;
+  }
+
+  public void setUpdatedAt(Date updatedAt) {
+    this.updatedAt = updatedAt;
   }
 
 }
